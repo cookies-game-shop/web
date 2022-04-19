670a9a24a1cdf722581aa44b9d9df1ef57a6c7a7
zhannaorazkan <muratkzhanna@gmail.com>
2022-04-19 15:21:53 +0600

    navbar design changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,49 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import {faCode} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 
 const Header =() =>{
     return(
-       <>
-           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-               <div className="container-fluid">
-                   <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                           data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                           aria-label="Toggle navigation">
-                   </button>
-                   <div className="collapse navbar-collapse" id="navbarNav">
-                       <ul className="navbar-nav">
-                           <li className="nav-item">
-                               <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
-                           </li>
-                           <li className="nav-item">
-                               <NavLink className="nav-link" to="#">Cart</NavLink>
-                           </li>
-                           <li className="nav-item">
-                               <NavLink className="nav-link" to="/about">About us</NavLink>
-                           </li>
-                           <li className="nav-item">
-                               <NavLink className="nav-link" to="/login">Login</NavLink>
-                           </li>
-                           <li className="nav-item">
-                               <NavLink className="nav-link" to="/signup">Register</NavLink>
-                           </li>
-                       </ul>
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
+                <div className="container">
+                    <a className="navbar-brand fw-bold fs-4" href="#">Game Store</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/about">About</NavLink>
+                            </li>
+                        </ul>
+                        <div className="buttons">
+                            <NavLink to="/login" className="btn btn-outline-dark">
+                                <i className="fa fa-sign-in me-1"></i>
+                                Login
+                            </NavLink>
+                            <NavLink to="/signup" className="btn btn-outline-dark ms-2">
+                                <i className="fa fa-user-plus me-1"></i>
+                                Register
+                            </NavLink>
+                            <NavLink to="" className="btn btn-outline-dark ms-2">
+                                <i className="fa fa-shopping-cart me-1"></i>
+                                Cart (0)
+                            </NavLink>
+                        </div>
 
-                   </div>
-                   <NavLink className="navbar-brand mx-auto fw-bold" to="/">Game Store</NavLink>
-               </div>
-           </nav>
-       </>
+                    </div>
+                </div>
+            </nav>
+        </div>
     )
 }
 
